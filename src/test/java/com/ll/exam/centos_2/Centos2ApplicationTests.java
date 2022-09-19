93886cf8da4a3a9f84d37689a47c6a8f6d967fe4
Jae-Soon Kwon <91954574+Jae-soon@users.noreply.github.com>
2022-09-19 23:00:41 +0900

    서버에서 빌드하기 위해 test 추가

--- src/test/java/com/ll/exam/centos_2/Centos2ApplicationTests.java
+++ src/test/java/com/ll/exam/centos_2/Centos2ApplicationTests.java
@@ -2,8 +2,10 @@ package com.ll.exam.centos_2;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class Centos2ApplicationTests {
 
     @Test
